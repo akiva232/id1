d495d8464cbfe694c073185d871d2529be275783
akiva <akiva232@gmail.com>
2020-07-15 18:36:34 +0300

    id1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { C1Component } from './c/c1/c1.component';
+import { C2Component } from './c/c2/c2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    C1Component,
+    C2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/s/s1.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class S1Service {
+
+  constructor() { }
+
+  counter:number=0;
+}
